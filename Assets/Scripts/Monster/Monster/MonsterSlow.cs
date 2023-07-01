06a3ffe008ea39a0e57441a910d695caa0f81fa9
leeeungu <74060680+leeeungu@users.noreply.github.com>
2023-07-01 18:46:34 +0900

    monster slow update

---
+++ Assets/Scripts/Monster/Monster/MonsterSlow.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSlow : MonoBehaviour
+{
+    public ParticleSystem particle;
+
+    public float totalDuration;
+
+    public bool bPlayBlackHole;
+    public bool isPlaying;
+    private void Start()
+    {
+        particle = GetComponent<ParticleSystem>();
+        StopPartical();
+    }
+    private void FixedUpdate()
+    {
+        // if (isPlaying)
+        //{
+        //    currentTime += Time.fixedDeltaTime;
+        //    if (currentTime > totalDuration)
+        //       StopPartical();
+        //}
+    }
+
+    public void PlayPartical() //외부에서 호출 해주면 실행 
+    {
+        if (isPlaying == true)
+            return;
+        isPlaying = true;
+        currentTime = 0;
+        particle.Play();
+    }
+    public void StopPartical()
+    {
+        isPlaying = false;
+        particle.Stop();
+        gameObject.SetActive(false);
+    }
+}
